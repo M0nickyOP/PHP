0ee953202f9b7072b0b4386cfa01fbc091073567
Monicky <164895217+M0nickyOP@users.noreply.github.com>
2024-11-14 08:47:44 -0300

    Add files via upload

---
+++ atv02/exploda.php
@@ -0,0 +1,25 @@
+<form method="post">
+<label> entre com uma lista com números separados por virgula para serem somados
+     <input type="text"  name="btn"> <br> 
+     <button type="submit" value="b1"> envia </button>
+</form>
+<?php 
+if ($_POST){
+    $a= $_POST["btn"];
+    $a = explode(",", $a);
+    $a = array_map('intval', $a);
+    $b = 0;
+    foreach($a as $index => $valor){
+        $b += $valor; 
+        
+        
+    }
+    echo " <div> $b </div> " ;
+}
+    
+#trazer a variável para o php
+#depois eu transformo em um vetor
+#usar p foreach para somar 
+
+
+?>
